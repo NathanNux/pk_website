5256f15359f44041a4bb79ea9721d7a8039e99ef
NathanNux <139380259+NathanNux@users.noreply.github.com>
2025-06-25 13:43:41 +0200

    adding google analytics script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,28 +1,33 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="cs">
       <Head>
-        {/* Google Analytics */}
-        <script 
-          async 
-          src="https://www.googletagmanager.com/gtag/js?id=G-LXSEHDDLM3"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-LXSEHDDLM3');
-            `,
-          }}
-        />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://syjlnusygybtuzoxirnw.supabase.co" />
+        <link rel="dns-prefetch" href="https://syjlnusygybtuzoxirnw.supabase.co" />
       </Head>
       <body>
         <Main />
         <NextScript />
+        
+        {/* Google Analytics with Next.js Script component */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LXSEHDDLM3"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LXSEHDDLM3');
+          `}
+        </Script>
       </body>
     </Html>
   );
