b8ce773e89309f1dffb28252da9a5af9b552454c
NathanNux <139380259+NathanNux@users.noreply.github.com>
2025-06-25 10:52:48 +0200

    added google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="cs">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script 
+          async 
+          src="https://www.googletagmanager.com/gtag/js?id=G-LXSEHDDLM3"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LXSEHDDLM3');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
